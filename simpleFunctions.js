c4450c60e67a16ee1b6b4b2826322b73e1685181
Abdufatoh <android17cpu@gmail.com>
2021-10-25 17:50:07 +0500

    add sum functions

--- simpleFunctions.js
+++ simpleFunctions.js
@@ -31,4 +31,26 @@ let salaries = {
 
 console.log(topSalary(salaries));
 
+/**
+ * Three functions with different implementation which return sum of given number
+ */
+
+function sumLoop(n) {
+    let s = 0;
+    for (let i = 1; i <= n; i++) {
+        s += i;
+    }
+    return s;
+}
+console.log(sumLoop(1))
+
+function sumRecursive(n) {
+    return n === 1 ? n : n + sum_recursive(n - 1)
+}
+console.log(sumRecursive(1))
+
+function sumArithmetic(n) {
+    return ((n + 1) / 2) * n
+}
+console.log(sumArithmetic(1))
 
